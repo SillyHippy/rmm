24cd01e5d578a320befa2b4a3db2f8cf8fb942ad
jojo8809 <159052837+SillyHippy@users.noreply.github.com>
2025-11-08 18:07:37 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import { ServiceAreas } from "@/components/ServiceAreas";
 import { WhyChooseUs } from "@/components/WhyChooseUs";
 import { ProcessServerFAQ } from "@/components/ProcessServerFAQ";
 import { CTASection } from "@/components/CTASection";
-import { StatsSection } from "@/components/StatsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <StatsSection />
       <Services />
       <ServiceAreas />
       <WhyChooseUs />
